19da8140e3d6eb3a3e46ac2e4e2c25b0a4caf74e
Elias Estrada <elicharles.e@gmail.com>
2025-06-05 20:02:25 -0700

    Remove target directory from git tracking

--- bitcell-program/target/debug/build/wasm-bindgen-shared-8ec6c280d6c7a5a7/build_script_build-8ec6c280d6c7a5a7.d
+++
@@ -1,5 +0,0 @@
-/Users/elicharlese/Documents/Elias Estrada/Professional/Minors/Bitcell/app/bitcell-program/target/debug/build/wasm-bindgen-shared-8ec6c280d6c7a5a7/build_script_build-8ec6c280d6c7a5a7: /Users/elicharlese/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-shared-0.2.100/build.rs
-
-/Users/elicharlese/Documents/Elias Estrada/Professional/Minors/Bitcell/app/bitcell-program/target/debug/build/wasm-bindgen-shared-8ec6c280d6c7a5a7/build_script_build-8ec6c280d6c7a5a7.d: /Users/elicharlese/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-shared-0.2.100/build.rs
-
-/Users/elicharlese/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-shared-0.2.100/build.rs:
